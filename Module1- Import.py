0b40bf8e96f4db24fd50c01746cc874535d251f4
Craig Cuninghame <craig3050@gmail.com>
2018-02-27 19:59:02 +0000

    Working open excel file, select sheet, print first 20 lines

--- Module1- Import.py
+++ Module1- Import.py
@@ -3,41 +3,6 @@ from openpyxl import load_workbook
 # import sqlite3
 # conn = sqlite3.connect('import_data.db')
 
-#Copy from previous working programme
-# def excel_list(file_path):
-#     drawing_list = Workbook()
-#     worksheet_1 = drawing_list.active
-#     drawing_list.save("Drawing_list.xlsx")
-#     row_number = 1
-#     for file_name in os.listdir(file_path):
-#         file_name_no_extension = ""
-#         for char in file_name:
-#             if char != ".":
-#                 file_name_no_extension += char
-#             else:
-#                 break
-#         file_extension = file_name.split(".")[-1]
-#         worksheet_1.cell(row = row_number, column = 1).value = file_name_no_extension
-#         worksheet_1.cell(row = row_number, column = 2).value = file_extension
-#         row_number += 1
-#     drawing_list.save("Drawing_list.xlsx")
-#     print("The drawing list has been written to the same directory as the script")
-#     print("Add your drawings into column C")
-#     #break before writing the new numbers
-#     continue_on()
-# drawing_list = load_workbook("Drawing_list.xlsx")
-# worksheet_1 = drawing_list.active
-# # rename the files
-# for i in range(1, row_number):
-#     old_file_name = worksheet_1.cell(row=i, column=1).value
-#     print(old_file_name)
-#     file_extension = worksheet_1.cell(row=i, column=2).value
-#     print(file_extension)
-#     new_file_name = worksheet_1.cell(row=i, column=3).value
-#     print(new_file_name)
-#     print("Old = " + old_file_name + ",  New = " + new_file_name)
-#     old_file_name = file_path + "/" + old_file_name + "." + file_extension
-#     RenameFile(old_file_name, new_file_name)
 
 #Part 1 - open the excel file
 def load_excel_file(file_path):
@@ -47,12 +12,20 @@ def load_excel_file(file_path):
     for i in workbook.get_sheet_names():
         sheet_names.append(i)
     print (sheet_names)
-    number = 1
+    number = 1 #Starting the list from one as it's easier for people to relate
     for item in sheet_names:
-        print (number + " - " + item)
-    sheet_select = input("Enter the number of the sheet to be imported: ")
-    sheet_select -=1
-    worksheet = sheet_names[sheet_select].active
+        print (str(number) + " - " + item)
+        number +=1
+    sheet_select = int(input("Enter the number of the sheet to be imported: "))
+    sheet_select -=1 #take 1 from the value as list addresses start from zero
+    worksheet = workbook[sheet_names[sheet_select]] # open the selected sheet
+    row_number = 1
+    while row_number <20:
+        print (worksheet.cell(row=row_number, column=1).value)
+        row_number +=1
+    input("Press Enter to exit")
+
+
 
 
 
